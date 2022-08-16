a8e2d231741b2fb18c5e4759ad8d4805af5153e7
Esau <montiel.orihuela.esau@gmail.com>
2022-08-16 12:24:30 -0500

    Adicion de comentarios al codigo

--- js/encrypter.js
+++ js/encrypter.js
@@ -1,7 +1,10 @@
+//Obtenemos los botones y campo de input y output
 var textField = document.getElementById("input");
 var btEncriptar = document.getElementById("encriptar");
 var btDesencriptar = document.getElementById("desencriptar");
 var textArea = document.getElementById("textArea");
+
+//Clves para la encriptacion de texto
 var keys = {
     "e": "enter",
     "i": "imes",
@@ -10,45 +13,53 @@ var keys = {
     "u": "ufat"
 }
 
+//Asiganr las funciones correspondientes a los botones encriptar y desencriptar
 btEncriptar.onclick = encriptar;
 btDesencriptar.onclick = desencriptar;
 
+//Valida que el texto ingresado solo contenga letras minusculas sin caracteres especiales
+//ademas, valida que el usuario haya ingresado algun valor
 function validateInput(texto)
 {
-    if(texto.length == 0) return false;
+    if(texto.length == "") return false;
     return(/^[a-z]*$/.test(texto));
 }
 
+//Encripta el texto
 function encriptar()
 {
-    var texto = textField.value;  
+    var texto = textField.value;//Obtiene el texto 
                     
-    if(validateInput(texto))
+    if(validateInput(texto))//Valida que el texto ingresado sea valido
     {
+        //Para cada una de las claves obtiene el valor por el que sera reeemplazado
         for(var letra in keys)
         {
+            //Reeemplaza la letra obtenida, por su valor encriptado
             texto = texto.replaceAll(letra, keys[letra]);
         }
 
-        textArea.value = texto;
-        textField.value = "";
+        textArea.value = texto;//Actualiza el text area con el texto encriptado
+        textField.value = "";//Limpia el text field
         textField.value = texto;
     }
 }
 
+//DEsencripta el texto
 function desencriptar()
 {
-    var texto = textField.value;  
+    var texto = textField.value;//Obtiene el texto   
                     
-    if(validateInput(texto))
+    if(validateInput(texto))//Valida que el texto ingresado sea valido
     {
         for(var letra in keys)
         {
+            //Reeemplaza el valor encriptado, por la letra correspondiente
             texto = texto.replaceAll(keys[letra], letra);
         }
 
-        textArea.value = texto;
-        textField.value = "";
+        textArea.value = texto;//Actualiza el text area con el texto desencriptado
+        textField.value = "";//Limpia el text field
         textField.value = texto;
     }
 }
