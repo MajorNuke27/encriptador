af9c315eda958e223ae678afe6c5de3386ec2851
Esau <montiel.orihuela.esau@gmail.com>
2022-08-20 13:04:56 -0500

    Correccion del enlace de los botones al js e implemntacion de las funciones copia y limpiar

--- js/encrypter.js
+++ js/encrypter.js
@@ -1,8 +1,10 @@
-//Obtenemos los botones y campo de input y output
-var textField = document.getElementById("input");
+//Obtenemos los elementos del DOM
+var textAreaInput = document.getElementById("input");
 var btEncriptar = document.getElementById("encriptar");
 var btDesencriptar = document.getElementById("desencriptar");
-var textArea = document.getElementById("textArea");
+var btLimpiar = document.getElementById("limpiar");
+var textAreaOutput = document.getElementById("textArea");
+var btCopiar = document.getElementById("copiar");
 
 //Claves para la encriptacion de texto
 var keys = {
@@ -13,22 +15,24 @@ var keys = {
     "u": "ufat"
 }
 
-//Asignar las funciones correspondientes a los botones encriptar y desencriptar
+//Asignar las funciones correspondientes a los botones
 btEncriptar.onclick = encriptar;
 btDesencriptar.onclick = desencriptar;
+btLimpiar.onclick = limpiar;
+btCopiar.onclick = copiar;
 
 //Valida que el texto ingresado solo contenga letras minusculas sin caracteres especiales
 //ademas, valida que el usuario haya ingresado algun valor
 function validateInput(texto)
 {
     if(texto == "") return false;
-    return(/^[a-z ]*$/.test(texto));
+    return(/^[a-z \n]*$/.test(texto));
 }
 
 //Encripta el texto
 function encriptar()
 {
-    var texto = textField.value;//Obtiene el texto 
+    var texto = textAreaInput.value;//Obtiene el texto 
                     
     if(validateInput(texto))//Valida que el texto ingresado sea valido
     {
@@ -39,16 +43,15 @@ function encriptar()
             texto = texto.replaceAll(letra, keys[letra]);
         }
 
-        textArea.value = texto;//Actualiza el text area con el texto encriptado
-        textField.value = "";//Limpia el text field
-        textField.value = texto;
+        textAreaOutput.value = texto;//Actualiza el text area con el texto encriptado
+        textAreaInput.value = "";//Limpia el text field
     }
 }
 
-//DEsencripta el texto
+//Desencripta el texto
 function desencriptar()
 {
-    var texto = textField.value;//Obtiene el texto   
+    var texto = textAreaInput.value;//Obtiene el texto   
                     
     if(validateInput(texto))//Valida que el texto ingresado sea valido
     {
@@ -58,8 +61,20 @@ function desencriptar()
             texto = texto.replaceAll(keys[letra], letra);
         }
 
-        textArea.value = texto;//Actualiza el text area con el texto desencriptado
-        textField.value = "";//Limpia el text field
-        textField.value = texto;
+        textAreaOutput.value = texto;//Actualiza el text area con el texto desencriptado
+        textAreaInput.value = "";//Limpia el text field
     }
+}
+
+function limpiar() {
+    textAreaInput.value = "";
+    textAreaOutput.value = "";
+}
+
+function copiar() {
+    navigator.clipboard.writeText(textAreaOutput.value).then((result) => {
+        console.log("Copiado al portapapeles");
+    }).catch((err) => {
+        console.log("Error al copiar");
+    });
 }
